c0215347a9810cd566accfa408674b136e5e8f97
Patrick Knöbel <pknoe3lh@gmail.com>
2019-07-01 16:55:29 +0200

    Added OLED library

--- Sketch/Sketch.ino
+++ Sketch/Sketch.ino
@@ -3,12 +3,36 @@
   Copyright (c) 2019 Patrick Knöbel
 */
 
+
+
 //we use GRBL as a library 
 #include "src/grbl/grbl.h"
 
+#include <Wire.h>
+#include "src/Adafruit_GFX_Library/Adafruit_GFX.h"
+#include "src/Adafruit_SSD1306/Adafruit_SSD1306.h"
+
+#define SCREEN_WIDTH 128 // OLED display width, in pixels
+#define SCREEN_HEIGHT 64 // OLED display height, in pixels
+
+// Declaration for an SSD1306 display connected to I2C (SDA, SCL pins)
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire);
+
+
+
 void setup()
 {
-    grbl_init();
+  grbl_init();
+  Wire.begin(16,15);
+
+  if(!display.begin(SSD1306_SWITCHCAPVCC, 0x3c)) { // Address 0x3D for 128x64
+    Serial.println("SSD1306 allocation failed");
+    for(;;); // Don't proceed, loop forever
+  }
+
+  display.clearDisplay();
+  display.display();
+  delay(2000); // Pause for 2 seconds
 }
 
 void loop()
@@ -16,5 +40,20 @@ void loop()
   grbl_service();
   
   //grbl_sendCMD("G0F10X0Y0");
+
   
+  display.setTextSize(1);             // Normal 1:1 pixel scale
+  display.setTextColor(WHITE);        // Draw white text
+  display.setCursor(0,0);             // Start at top-left corner
+  display.println(F("Hello, world!"));
+  display.display();
+
+  delay(1000);
+  display.setTextSize(1);             // Normal 1:1 pixel scale
+  display.setTextColor(BLACK);        // Draw white text
+  display.setCursor(0,0);             // Start at top-left corner
+  display.println(F("Hello, world!"));
+  display.display();
+  
+  delay(1000);
 }
